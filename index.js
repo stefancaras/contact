00faabddd847dcf5f776341c95668f78f277f9ea
Stefan Caras <stefan.caras@gmail.com>
2022-08-28 15:03:41 +0300

    update

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 let name = document.getElementById('name');
-name.addEventListener('invalid', function(){
-    name.classList.add("invalid");
-})
-name.addEventListener('keydown', function(){
-    name.classList.remove("invalid");
-})
-
 let email = document.getElementById('email');
-email.addEventListener('invalid', function(){
-    email.classList.add("invalid");
-})
-email.addEventListener('keydown', function(){
-    email.classList.remove("invalid");
-})
-
 let radio1 = document.getElementById('radio1');
 let radio2 = document.getElementById('radio2');
-radio1.addEventListener('click', function(){
-    radio1.classList.remove("invalid");
-    radio2.classList.remove("invalid");
-})
-radio2.addEventListener('click', function(){
-    radio1.classList.remove("invalid");
-    radio2.classList.remove("invalid");
-})
-
 let message = document.getElementById('message');
-message.addEventListener('invalid', function(){
-    message.classList.add("invalid");
-})
-message.addEventListener('keydown', function(){
-    message.classList.remove("invalid");
-})
-
 let button = document.getElementById('button');
 let thanks = document.getElementById('thanks');
 let namearr = [];
 button.addEventListener('click', function(){
-    if (!radio1.checked && !radio2.checked) {
-        radio1.classList.add("invalid");
-        radio2.classList.add("invalid");
-    }
     if (name.checkValidity() && email.checkValidity() && 
     message.checkValidity() && (radio1.checked || radio2.checked)) {
         console.log(`Nume și prenume: ${name.value}`);
@@ -52,7 +18,7 @@ button.addEventListener('click', function(){
         }
         console.log(`Mesaj: ${message.value}`);
         namearr = name.value.split(" ");
-        name.value=email.value=message.value=null;
+        name.value=email.value=message.value="";
         radio1.checked=radio2.checked=false;
         thanks.classList.add("green");
         thanks.innerHTML = `<i class="fas fa-check"></i>Informațiile tale au fost trimise.<br>
@@ -62,6 +28,40 @@ button.addEventListener('click', function(){
         x.addEventListener('click', function(){
             thanks.innerHTML = "";
             thanks.classList.remove("green");
+        })
+    } else {
+        if (name.checkValidity() == false) {
+                name.classList.add("invalid");
+        }
+        name.addEventListener('keydown', function(){
+                name.classList.remove("invalid");
+        })
+        
+        if (email.checkValidity() == false) {
+                email.classList.add("invalid");
+        }
+        email.addEventListener('keydown', function(){
+            email.classList.remove("invalid");
+        })
+        
+        if (!radio1.checked && !radio2.checked) {
+            radio1.classList.add("invalid");
+            radio2.classList.add("invalid");
+        }
+        radio1.addEventListener('click', function(){
+            radio1.classList.remove("invalid");
+            radio2.classList.remove("invalid");
+        })
+        radio2.addEventListener('click', function(){
+            radio1.classList.remove("invalid");
+            radio2.classList.remove("invalid");
+        })
+        
+        if (message.checkValidity() == false) {
+                message.classList.add("invalid");
+        }
+        message.addEventListener('keydown', function(){
+            message.classList.remove("invalid");
         })
     }
 })
