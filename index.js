c8b0a0c485bac16e7365a41dc9e2b27be0ccff90
Stefan Caras <stefan.caras@gmail.com>
2022-09-17 09:31:28 +0300

    updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let name = document.getElementById('name');
+let contactName = document.getElementById('contactName');
 let email = document.getElementById('email');
 let radio1 = document.getElementById('radio1');
 let radio2 = document.getElementById('radio2');
@@ -7,9 +7,10 @@ let button = document.getElementById('button');
 let thanks = document.getElementById('thanks');
 
 button.addEventListener('click', function(){
-    if (name.checkValidity() && email.checkValidity() && 
+    //If everything is fine, show info in console
+    if (contactName.checkValidity() && email.checkValidity() && 
     message.checkValidity() && (radio1.checked || radio2.checked)) {
-        console.log(`Nume și prenume: ${name.value}`);
+        console.log(`Nume și prenume: ${contactName.value}`);
         console.log(`E-mail: ${email.value}`);
         if (radio1.checked) {
             console.log(`Persoană fizică`);
@@ -17,43 +18,48 @@ button.addEventListener('click', function(){
             console.log(`Persoană juridică`);
         }
         console.log(`Mesaj: ${message.value}`);
-
-        let namearray = name.value.split(" ");
-        name.value=email.value=message.value="";
+        let namearray = contactName.value.split(" ");
+        //Clear input fields
+        contactName.value=email.value=message.value="";
         radio1.checked=radio2.checked=false;
+        //Thank you message
         thanks.classList.add("green");
         thanks.innerHTML = `<i class="fas fa-check"></i>
             Informațiile tale au fost trimise.<br>
             Mulțumesc, ${namearray[namearray.length-1]}. 
             <i class="fas fa-window-close" id="x"></i>`;
-        
+        //Close button for thank you message
         let x = document.getElementById('x');
-        x.addEventListener('click', function(){
+        x.addEventListener('click', function() {
             thanks.innerHTML = "";
             thanks.classList.remove("green");
         })
+    //If something is wrong
     } else {
-        const elemArray = [name, email, message];
+        //Check what is wrong and add class invalid to elements
+        const elemArray = [contactName, email, message];
         for (let elem of elemArray) {
-            if (elem.checkValidity() == false) {
+            if (elem.checkValidity() === false) {
                 elem.classList.add("invalid");
             }
+            //Remove class invalid if input is fine
             elem.addEventListener('keyup', function(){
                 if (elem.checkValidity()) {
                     elem.classList.remove("invalid");
                 }
             })
         }
-        
+        //Check what is wrong and add class invalid to radio buttons
         if (!radio1.checked && !radio2.checked) {
             radio1.classList.add("invalid");
             radio2.classList.add("invalid");
         }
-        radio1.addEventListener('click', function(){
+        //Remove class invalid if input is fine
+        radio1.addEventListener('click', function() {
             radio1.classList.remove("invalid");
             radio2.classList.remove("invalid");
         })
-        radio2.addEventListener('click', function(){
+        radio2.addEventListener('click', function() {
             radio1.classList.remove("invalid");
             radio2.classList.remove("invalid");
         })
